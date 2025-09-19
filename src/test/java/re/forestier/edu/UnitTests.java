74c995cd28692b2319df72dfd15cbc73ef33fa9b
magoujon1 <magoujon1@a206pc10l.local.isima.fr>
2025-09-19 15:24:53 +0200

    Ajout des Test pour la classe joueur pas encore totalement fini

--- src/test/java/re/forestier/edu/UnitTests.java
+++ src/test/java/re/forestier/edu/UnitTests.java
@@ -42,12 +42,70 @@ public class UnitTests {
         assertThat(p.inventory, hasItem("epee"));
     }
 
-    // Teste de la classe Affichage par instaciation
+    // Test de la classe Affichage par instaciation
     @Test
     @DisplayName("Test Affichage")
     void testAffichage() {
         new Affichage();
     } 
 
-    
+    // Test de la méthode getXP() de la classe player
+    @Test
+    @DisplayName("XP getter")
+    void testGetXP() {
+        player p = new player("Maxou", "Le mec la avec ses cartes", "ADVENTURER", 100, new ArrayList<>());
+        int xp = p.getXp();
+        assertThat(xp, is(0));
+    }   
+
+    // Test des différentes classes possibles pour un joueur
+    @Test
+    @DisplayName("player class")
+    void testClass() {
+        player p1 = new player("Pierre", "Pierro", "ARCHER", 101, new ArrayList<>());
+        player p2 = new player("Paul", "Paulo", "ADVENTURER", 202, new ArrayList<>());
+        player p3 = new player("Jacques", "Jaco", "DWARF", 303, new ArrayList<>());
+        player p4 = new player("Machin", "Machino", "", 404, new ArrayList<>());
+        assertThat(p1.getAvatarClass(), is("ARCHER"));
+        assertThat(p2.getAvatarClass(), is("ADVENTURER"));
+        assertThat(p3.getAvatarClass(), is("DWARF"));
+        assertThat(p4.getAvatarClass(), is(nullValue()));
+    }
+
+    // Test pour enlever de l'argent à un joueur 
+    @Test
+    @DisplayName("Possible to have positive money")
+    void testPositiveMoney() {
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+
+        try {
+            p.removeMoney(50);
+        } catch (Exception e) {
+            assertThat(e, is(instanceOf(IllegalArgumentException.class)));
+        }
+    }
+
+     // Test d'ajouter de l'argent à un joueur
+    @Test
+    @DisplayName("Adding money")
+    void testAddMoney() {
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+
+        try {
+            p.addMoney(50);
+        } catch (Exception e) {
+            assertThat(e, is(instanceOf(IllegalArgumentException.class)));
+        }
+    }
+
+    // Test d'ajouter de l'argent null à un joueur ne change pas son argent
+    @Test
+    @DisplayName("addMoney(0) does not change money")
+    void testAddNoMoney() {
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+        int initialMoney = p.money;
+        p.addMoney(0);
+        assertThat(p.money, is(initialMoney));
+    }
+
 }
