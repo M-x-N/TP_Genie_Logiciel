5e7b3c865bfde72fdef1a64b8af6e403210940ec
magoujon1 <magoujon1@a206pc10l.local.isima.fr>
2025-09-26 11:59:12 +0200

    Tests de la classe UpdatePlayer.java

--- src/test/java/re/forestier/edu/UnitTests.java
+++ src/test/java/re/forestier/edu/UnitTests.java
@@ -3,6 +3,7 @@ package re.forestier.edu;
 import org.junit.jupiter.api.*;
 
 import re.forestier.edu.rpg.Affichage;
+import re.forestier.edu.rpg.UpdatePlayer;
 import re.forestier.edu.rpg.player;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -119,4 +120,108 @@ public class UnitTests {
         assertThat(p.money, is(initialMoney - 25));
     }
 
+    @Test
+    @DisplayName("Test retrieve player level based on XP")
+    void testRetrievePlayerLevel() {
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+ 
+        UpdatePlayer.addXp(p, 0);  
+        assertThat(p.retrieveLevel(), is(1));
+        
+        UpdatePlayer.addXp(p, 10); 
+        assertThat(p.retrieveLevel(), is(2));
+
+        UpdatePlayer.addXp(p, 27); 
+        assertThat(p.retrieveLevel(), is(3));
+        
+        UpdatePlayer.addXp(p, 57); 
+        assertThat(p.retrieveLevel(), is(4));
+        
+        UpdatePlayer.addXp(p, 111); 
+        assertThat(p.retrieveLevel(), is(5));
+    }
+
+    // Test de la classe UpdatePlayer par instaciation
+    @Test
+    @DisplayName("Test UpdatePlayer")
+    void testUpdatePlayer() {
+        new UpdatePlayer();
+    } 
+
+
+    // Test pour voir si le joueur est vivant 1 point de vie ou plus
+    @Test
+    @DisplayName("Test Player is Alive")
+    void testAlivePlayer(){
+        player p1 = new player("Pierre", "Pierro", "ARCHER", 101, new ArrayList<>());
+        player p2 = new player("Paul", "Paulo", "ADVENTURER", 202, new ArrayList<>());
+        player p3 = new player("Jacques", "Jaco", "DWARF", 303, new ArrayList<>());
+
+        // Low Life
+        p1.currenthealthpoints = 1;
+        p1.healthpoints = 1000;
+        p2.currenthealthpoints = 1;
+        p2.healthpoints = 100;
+        p3.currenthealthpoints = 1;
+        p3.healthpoints = 1000;
+        UpdatePlayer.majFinDeTour(p1);
+        UpdatePlayer.majFinDeTour(p2);
+        UpdatePlayer.majFinDeTour(p3);
+
+        // Mid Life
+        p1.currenthealthpoints = 500;
+        p1.healthpoints = 1000;
+        p2.currenthealthpoints = 500;
+        p2.healthpoints = 100;
+        p3.currenthealthpoints = 500;
+        p3.healthpoints = 1000;
+        UpdatePlayer.majFinDeTour(p1);
+        UpdatePlayer.majFinDeTour(p2);
+        UpdatePlayer.majFinDeTour(p3);
+
+        // Full Life
+        p1.currenthealthpoints = 1000;
+        p1.healthpoints = 1000;
+        p2.currenthealthpoints = 1000;
+        p2.healthpoints = 100;
+        p3.currenthealthpoints = 1000;
+        p3.healthpoints = 1000;
+        UpdatePlayer.majFinDeTour(p1);
+        UpdatePlayer.majFinDeTour(p2);
+        UpdatePlayer.majFinDeTour(p3);
+    }
+
+    // Test pour voir si le joueur est mort (0 point de vie)
+    @Test
+    @DisplayName("Test Dead Player")
+    void testDeadPlayer(){
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+        UpdatePlayer.majFinDeTour(p);
+    }
+
+    // Test des inventaires pour les classes DWARF et ARCHER
+    @Test
+    @DisplayName("Test Inventory for DWARF and ARCHER")
+    void testInventoryPlayer(){
+        player p1 = new player("Pierre", "Pierro", "DWARF", 101, new ArrayList<>());
+        player p2 = new player("Paul", "Paulo", "ARCHER", 202, new ArrayList<>());
+        player p3 = new player("Jacques", "Jaco", "ADVENTURER", 303, new ArrayList<>());
+
+        p1.currenthealthpoints = 400;
+        p1.healthpoints = 1000;
+        p2.currenthealthpoints = 400;
+        p2.healthpoints = 1000;
+
+        p3.currenthealthpoints = 1000;
+        p3.healthpoints = 400;
+
+        p1.inventory.add("Holy Elixir");
+        p2.inventory.add("Magic Bow");
+
+        UpdatePlayer.majFinDeTour(p1);
+        UpdatePlayer.majFinDeTour(p2);
+        UpdatePlayer.majFinDeTour(p3);
+    }    
+
 }
+    
